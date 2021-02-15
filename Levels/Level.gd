6c42d8bae41dbd277eca78893f1c3d9325eea493
Marcelo Mandaji <marcelomandaji@gmail.com>
2021-02-15 15:03:36 -0300

    camera cycle smooth change

--- Levels/Level.gd
+++ Levels/Level.gd
@@ -5,9 +5,27 @@ var camera_is_on_player = true
 var finishing = false
 
 func cycle_camera():
+	
 	if camera_is_on_player && get_node("PlayerClone") != null:
+		
 		camera_is_on_player = false
+		
+		get_node("PlayerClone").get_node("Camera2D").smoothing_enabled = false
+		get_node("PlayerClone").get_node("Camera2D").position.x = get_node("Player").position.x - get_node("PlayerClone").position.x
+		get_node("PlayerClone").get_node("Camera2D").position.y = get_node("Player").position.y - get_node("PlayerClone").position.y
 		get_node("PlayerClone").set_camera()
+		get_node("PlayerClone").get_node("Camera2D").smoothing_enabled = true
+		get_node("PlayerClone").get_node("Camera2D").position.x = 0
+		get_node("PlayerClone").get_node("Camera2D").position.y = 0
+		
 	else:
+		
 		camera_is_on_player = true
+		
+		get_node("Player").get_node("Camera2D").smoothing_enabled = false
+		get_node("Player").get_node("Camera2D").position.x = get_node("PlayerClone").position.x - get_node("Player").position.x
+		get_node("Player").get_node("Camera2D").position.y = get_node("PlayerClone").position.y - get_node("Player").position.y
 		get_node("Player").set_camera()
+		get_node("Player").get_node("Camera2D").smoothing_enabled = true
+		get_node("Player").get_node("Camera2D").position.x = 0
+		get_node("Player").get_node("Camera2D").position.y = 0
