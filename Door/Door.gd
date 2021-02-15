476427087960dc4cc5c2faecdb78548a17fd5dba
Marcelo Mandaji <marcelomandaji@gmail.com>
2021-02-15 13:30:30 -0300

    camera bug fix

--- Door/Door.gd
+++ Door/Door.gd
@@ -1,7 +1,8 @@
 extends AnimatedSprite
 
 var body_entered = null
-var max_levels = 3
+var max_levels = 2
+
 func _ready():
 	pass
 	
@@ -11,16 +12,16 @@ func _on_Hitbox_body_entered(body):
 			self.play("Open")
 			body_entered = body
 			get_node("../").finishing = true
-		else:
-			print("door closed")
+
 
 
 func _on_AnimatedSprite_animation_finished():
 	if self.animation == "Open":
-		body_entered.queue_free()
+		#body_entered.queue_free()
+		body_entered.get_node("AnimatedSprite").modulate.a = 0
 		self.play("Finish")
 	elif self.animation == "Finish":
-		if (int(get_tree().current_scene.name) + 1) < max_levels:
+		if (int(get_tree().current_scene.name) + 1) <= max_levels:
 			get_tree().change_scene("res://Levels/Level_" + str(int(get_tree().current_scene.name) + 1) + ".tscn")
 		else:
 			get_tree().change_scene("res://Levels/Level_1.tscn")
--- Itens/KeyItem.gd
+++ Itens/KeyItem.gd
@@ -1,6 +1,5 @@
 extends AnimatedSprite
 
 func _on_Hitbox_body_entered(body):
-	print('got key')
 	get_node("../").got_key = true
 	queue_free()
