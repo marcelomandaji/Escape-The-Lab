7c02d5866648f4e8e56dc4d3b1754f330640ebe7
Marcelo Mandaji <marcelomandaji@gmail.com>
2021-02-16 00:33:33 -0300

    level 4 - end of 3rd day

--- Itens/BlueSwitch.gd
+++ Itens/BlueSwitch.gd
@@ -5,4 +5,15 @@ signal BlueSwitch
 func _on_Hitbox_body_entered(body):
 	if body.is_in_group("Player"):
 		emit_signal("BlueSwitch")
-		queue_free()
+		$Hitbox/CollisionShape2D.set_deferred("disabled", true)
+		self.play("Apertado")
+
+
+func _on_RedSwitch_RedSwitch():
+	$Hitbox/CollisionShape2D.set_deferred("disabled", false)
+	self.play("Idle")
+
+
+func _on_BlueSwitch_BlueSwitch():
+	$Hitbox/CollisionShape2D.set_deferred("disabled", true)
+	self.play("Apertado")
--- Itens/OnOffDoor.gd
+++ Itens/OnOffDoor.gd
@@ -1,13 +1,13 @@
 extends AnimatedSprite
 
 func _ready():
-	self.modulate.a = 0
+	pass
 	
 func _on_BlueSwitch_BlueSwitch():
 	$StaticBody2D/CollisionShape2D.set_deferred("disabled", true)
-	self.modulate.a = 0
+	self.play("Open")
 
 
 func _on_RedSwitch_RedSwitch():
 	$StaticBody2D/CollisionShape2D.set_deferred("disabled", false)
-	self.modulate.a = 1
+	self.play("Closed")
